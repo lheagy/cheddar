b72545654bad030a8593c3f87b499ecd59045c3d
Lindsey Heagy <lindseyheagy@gmail.com>
2017-12-31 14:15:15 -0700

    add docstrings for library

--- saycheese/library.py
+++ saycheese/library.py
@@ -9,6 +9,7 @@ IMAGE_EXTENSION = ["jpg", "png"]
 VIDEO_EXTENSION = ["mp4"]
 
 IGNORE_STR = "._"
+SAYCHEESEINFO = '.saycheese'
 
 
 class Library(properties.HasProperties):
@@ -22,6 +23,7 @@ class Library(properties.HasProperties):
     def __init__(self, directory):
         super(Library, self).__init__()
         self.directory = directory
+        if os.isfile(self.directory + os.path.sep + )
 
     @properties.validator('directory')
     def _ensure_abspath(self, change):
@@ -43,6 +45,12 @@ class Library(properties.HasProperties):
 
     @property
     def media(self):
+        """
+        media objects in the library
+
+        :rtype: saycheese.Media
+        :return: saycheese Media object
+        """
         if getattr(self, '_media', None) is None:
             files = os.listdir(self.directory)
             self._media = [
@@ -55,10 +63,22 @@ class Library(properties.HasProperties):
 
     @property
     def media_names(self):
+        """
+        filenames for each of the media items in the library
+
+        :rtype: list
+        :return: list of strings of the filenames for each media item in the library
+        """
         return [m.name for m in self.media]
 
     @property
     def videos(self):
+        """
+        media object for each of the videos in the library
+
+        :rtype: list
+        :return: list of :class:saycheese.Media items for each video
+        """
         if getattr(self, '_videos', None) is None:
             self._videos = [
                 m for m in self.media if
@@ -68,10 +88,22 @@ class Library(properties.HasProperties):
 
     @property
     def video_names(self):
+        """
+        filenames for each of the videos in the library
+
+        :rtype: list
+        :return: list of strings of the filenames for each video in the library
+        """
         return [m.name for m in self.videos]
 
     @property
     def images(self):
+        """
+        media object for each of the simage in the library
+
+        :rtype: list
+        :return: list of :class:saycheese.Media items for each image
+        """
         if getattr(self, '_images', None) is None:
             self._images = [
                 m for m in self.media if
@@ -81,9 +113,18 @@ class Library(properties.HasProperties):
 
     @property
     def image_names(self):
+        """
+        filenames for each of the image in the library
+
+        :rtype: list
+        :return: list of strings of the filenames for each image in the library
+        """
         return [m.name for m in self.images]
 
     def open_images(self):
+        """
+        open the images in the library
+        """
         images = [
             self.directory + os.path.sep + img for img in self.image_names
         ]
@@ -98,6 +139,9 @@ class Library(properties.HasProperties):
                     raise Exception("Couldn't open images")
 
     def open_videos(self):
+        """
+        open the videos in the library
+        """
         videos = [
             self.directory + os.path.sep + vid for vid in self.video_names
         ]
@@ -112,6 +156,9 @@ class Library(properties.HasProperties):
                     raise Exception("Couldn't open videos")
 
     def open(self):
+        """
+        open the images and the videos in the library
+        """
         self.open_images()
         self.open_videos()
 
@@ -166,6 +213,9 @@ class Library(properties.HasProperties):
         filename_format="%Y-%m-%d %H.%M.%S",
         verbose=True
     ):
+        """
+        rename the content in the library by date
+        """
         for m in self.media:
             m.rename_by_date(
                 lowercase_extension,
